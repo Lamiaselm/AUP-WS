77fda302c1a8e79732ae2ae76ab43490483bd69f
lamiasln <lamia_sn@outlook.fr>
2020-03-12 22:43:54 +0100

    home page done

--- src/App.js
+++ src/App.js
@@ -4,15 +4,15 @@ import './App.css';
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Login from './Components/Login';
 import SignUp from './Components/SignUp';
+import Home from './Components/Home';
 
 function App() {
   return (
     <Router>
-    <div className="App">
-
-
-  
-    </div></Router>
+    <div className="App">  
+    <Home/>
+    </div>
+    </Router>
   );
 }
 
---
+++ src/Components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Home extends React.Component{
+    render (){
+        return (
+            <div className="accueil">
+        <form class="cf" action="./home"><input className="start" type="submit" value="GET STARTED !" id="input-submit"/></form>
+
+            </div>
+
+        );
+    }
+}
+export default Home;
