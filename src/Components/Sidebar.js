f3a54c0addb3c42e642e2e19537c0705fb28e08e
lamiasln <lamia_sn@outlook.fr>
2020-03-30 04:25:02 +0200

    admin sidebar done

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -1,10 +1,31 @@
 import React from 'react';
 import logo from '../Assets/AUPLOGO.png'
+import { getProfile } from '../Components/UserFunctions'
 import { slide as Menu } from 'react-burger-menu';
+class Sidebar extends React.Component{
+  constructor() {
+    super()
+    this.state = {
+        nom: '',
+        prenom:'',
+        email: ''
+    }
+}
 
-export default props => {
-  const {email}=localStorage.getItem('usertoken');
-  console.log(email);
+componentDidMount() {
+    getProfile().then(res => {
+        this.setState({
+            nom: res.user.nom,
+            prenom: res.user.prenom,
+            email: res.user.email
+        })
+        var email =this.state.email
+        console.log(email)
+      
+    })
+}
+render(){
+  
   return (
     <Menu>
         <img src={logo} height="150px" style={{marginLeft:"27px" }}></img>
@@ -26,13 +47,16 @@ export default props => {
       <a className="menu-item" href="/Team">
       Team
       </a>
-      <a className="menu-item" href="/table">
-       Answers
-      </a>
+      {this.state.email=="hl_selmane@esi.dz" &&  <a className="menu-item" href="/table">Answers </a>}
+
+
+     
 
       <a className="menu-item" href="/Deconexion">
      Deconexion
       </a>
     </Menu>
   );
-};
+}
+}
+export default Sidebar;
